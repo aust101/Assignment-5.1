d2e41903f65b68aeeea58d62b7e479f1e1696cf2
aust101 <31296022+aust101@users.noreply.github.com>
2021-01-21 12:52:06 -0600

    Add files via upload

---
+++ assignment_5_1/Main.java
@@ -0,0 +1,8 @@
+package com.overaitis.assignment_5_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(8,11);
+        System.out.println(rectangle.toString());
+    }
+}
---
+++ assignment_5_1/Rectangle.java
@@ -0,0 +1,37 @@
+package com.overaitis.assignment_5_1;
+
+public class Rectangle {
+
+    private final double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return (2*width) + (2*height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                ", area=" + getArea() +
+                ", perimeter=" + getPerimeter() +
+                '}';
+    }
+}
